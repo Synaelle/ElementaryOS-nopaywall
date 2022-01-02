e19f935aec3138e4d6b33fcf8e70c6c3a74598a6
Synaelle <alexisdu69100@gmail.com>
2022-01-02 11:40:01 +0100

    add comment

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ElementaryOS - Remove paywall
 // @namespace    https://github.com/Synaelle/ElementaryOS-nopaywall
-// @version      1.0
+// @version      1.01
 // @description  It removes the fake paywall on ElementaryOS website
 // @author       Synaelle
 // @match        https://elementary.io/*
@@ -15,6 +15,7 @@
     document.getElementById("payment-trust").style.display = 'none';
     document.getElementById("choice-buttons").style.display = 'none';
     document.getElementById("pay-what-you-want").style.display = 'none';
+    //If you don't choose any value, they will put 20$ if you click buy. So that's how I found a workaround for this.
     document.getElementById("amount-twenty").value = '0';
     document.getElementById('translate-purchase').innerHTML = document.getElementById('translate-download').innerHTML;
 }
